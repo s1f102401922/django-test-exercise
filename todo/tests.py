b59b10505f0f47e8c3c7ddca43a6dc4553e24208
1F10240192 <s1f102401922@iniad.org>
2025-07-03 13:44:48 +0900

    flake8の修正

--- todo/tests.py
+++ todo/tests.py
@@ -3,10 +3,11 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
-# Create your tests here.
+
 class SampleTestCase(TestCase):
     def test_sample(self):
         self.assertEqual(1 + 2, 3)
+
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
@@ -50,6 +51,7 @@ class TaskModelTestCase(TestCase):
         task.save()
 
         self.assertFalse(task.is_overdue(current))
+
 
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
